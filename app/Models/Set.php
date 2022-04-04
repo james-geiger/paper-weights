0cd819f7c9f798bff56cc893a662bbf50b6756da
James Geiger <james-geiger@users.noreply.github.com>
2022-04-04 00:11:47 -0500

    bug fix

--- app/Models/Set.php
+++ app/Models/Set.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Traits\Uuids;
