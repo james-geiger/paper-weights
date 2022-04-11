147509bf8b530ae6350a81f3db57d30b824b7803
James Geiger <james-geiger@users.noreply.github.com>
2022-04-10 21:02:20 -0500

    Fix layout on prev/next

--- resources/js/Components/Log/ShowHeader.vue
+++ resources/js/Components/Log/ShowHeader.vue
@@ -38,7 +38,7 @@
         </div>
         <div class="mt-5 flex flex-row justify-between sm:justify-start space-x-2 lg:mt-0 lg:ml-4">
             <span class="block lg:ml-3 w-1/2 lg:w-auto text-left">
-                <a :href="pagination.prev" type="button" v-if="pagination.prev">
+                <a :href="pagination.prev" type="button" v-if="pagination.prev" class="w-full">
                     <button
                     class="w-full inline-flex justify-start items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     <ChevronLeftIcon class="-ml-1 mr-2 h-5 w-5 text-gray-500" aria-hidden="true" />
@@ -53,7 +53,7 @@
             </span>
 
             <span class="block lg:ml-3 w-1/2 lg:w-auto text-right">
-                <a :href="pagination.next" type="button" v-if="pagination.next">
+                <a :href="pagination.next" type="button" v-if="pagination.next" class="w-full">
                 <button
                     class="w-full inline-flex justify-end items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     Next Exercise
